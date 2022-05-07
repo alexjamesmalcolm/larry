922f1b89e57d4d6d5ff27e2ee889935eddd32894
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2022-05-07 15:45:06 -0400

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import GraphicDesignGallery from "./pages/GraphicDesignGallery";
 import IllustrationGallery from "./pages/IllustrationGallery";
 
 const App = () => (
-  <Routes>
-    <Route index element={<IllustrationGallery />} />
-    <Route path="/graphic-design" element={<GraphicDesignGallery />} />
-  </Routes>
+  <div>
+    <header>
+      <h1>Larry Mitchell</h1>
+    </header>
+    <Routes>
+      <Route index element={<IllustrationGallery />} />
+      <Route path="/graphic-design" element={<GraphicDesignGallery />} />
+    </Routes>
+  </div>
 );
 
 export default App;
