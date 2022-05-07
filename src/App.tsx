50853a6008db9d655d8d6260783b06f1d6de637b
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2022-05-07 16:21:50 -0400

    Added modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, NavLink } from "react-router-dom";
 import GraphicDesignGallery from "./pages/GraphicDesignGallery";
 import IllustrationGallery from "./pages/IllustrationGallery";
+import styles from "./App.module.css";
+import classNames from "classnames";
 
 const App = () => (
   <div>
-    <header>
+    <header className={styles.header}>
       <h1>Larry Mitchell</h1>
+      <nav className={styles.navigation}>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                classNames(styles.link, { [styles.active]: isActive })
+              }
+            >
+              Illustrations
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="graphic-design">Graphic Design</NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
     <Routes>
       <Route index element={<IllustrationGallery />} />
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
 import styles from "./Gallery.module.css";
 
 export interface Content {
@@ -7,15 +9,35 @@ export interface Content {
   name: string;
 }
 
-const Gallery = ({ content }: { content: Content[] }) => (
-  <div className={styles.container}>
-    {content.map(({ id, src, description, name }) => (
-      <article key={id} className={styles.imageContainer}>
-        <img className={styles.image} src={src} alt={description} />
-        <p className={styles.name}>{name}</p>
-      </article>
-    ))}
-  </div>
-);
+const Gallery = ({ content }: { content: Content[] }) => {
+  const [activeImageId, setActiveImageId] = useState<Content["id"]>();
+  return (
+    <div className={styles.container}>
+      {content.map(({ id, src, description, name }) => (
+        <>
+          <article
+            key={id}
+            className={styles.galleryContainer}
+            onClick={() => setActiveImageId(id)}
+          >
+            <img className={styles.image} src={src} alt={description} />
+            <p className={styles.name}>{name}</p>
+          </article>
+          <Modal
+            show={activeImageId === id}
+            size="xl"
+            onHide={() => setActiveImageId(undefined)}
+            className={styles.modal}
+          >
+            <Modal.Header>Hi</Modal.Header>
+            <Modal.Body>
+              <img src={src} alt={description} className={styles.image} />
+            </Modal.Body>
+          </Modal>
+        </>
+      ))}
+    </div>
+  );
+};
 
 export default Gallery;
