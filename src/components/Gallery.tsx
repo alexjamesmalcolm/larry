10d54b804cd43be368bb6e62f96e13ade73c01a3
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2022-05-08 22:26:52 -0400

    Made Lightbox much better

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,13 @@
-import { Fragment, useCallback, useMemo, useState } from "react";
+import { Fragment, useCallback, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./Gallery.module.css";
+import ISwiper, { Navigation, Pagination, Keyboard, Mousewheel } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export interface Content {
   src: string;
@@ -11,63 +17,61 @@ export interface Content {
 }
 
 const Gallery = ({ content }: { content: Content[] }) => {
-  const [activeImageId, setActiveImageId] = useState<Content["id"]>();
-  const close = useCallback(() => setActiveImageId(undefined), []);
-  const lastContentIndex = useMemo(() => content.length - 1, [content.length]);
-  const previous = useCallback(() => {
-    const currentContentIndex = content.findIndex(
-      ({ id }) => id === activeImageId
-    );
-    if (currentContentIndex === undefined) {
-      close();
-    } else if (currentContentIndex === 0) {
-      setActiveImageId(content[lastContentIndex].id);
-    } else {
-      setActiveImageId(content[currentContentIndex - 1].id);
-    }
-  }, [activeImageId, close, content, lastContentIndex]);
-  const next = useCallback(() => {
-    const currentContentIndex = content.findIndex(
-      ({ id }) => id === activeImageId
-    );
-    if (currentContentIndex === undefined) {
-      close();
-    } else if (currentContentIndex === lastContentIndex) {
-      setActiveImageId(content[0].id);
-    } else {
-      setActiveImageId(content[currentContentIndex + 1].id);
-    }
-  }, [activeImageId, close, content, lastContentIndex]);
+  const [activeImageIndex, setActiveImageIndex] = useState<number | undefined>(
+    undefined
+  );
+  const close = useCallback(() => setActiveImageIndex(undefined), []);
+  const onSwiperCreation = useCallback(
+    (swiper: ISwiper) => {
+      if (activeImageIndex) {
+        swiper.slideTo(activeImageIndex, 0);
+      }
+    },
+    [activeImageIndex]
+  );
   return (
-    <div className={styles.container}>
-      {content.map(({ id, src, description, name }) => (
-        <Fragment key={id}>
-          <article
-            className={styles.galleryContainer}
-            onClick={() => setActiveImageId(id)}
+    <>
+      <div className={styles.container}>
+        {content.map(({ id, src, description }, index) => (
+          <Fragment key={id}>
+            <article
+              className={styles.galleryContainer}
+              onClick={() => setActiveImageIndex(index)}
+            >
+              <img className={styles.image} src={src} alt={description} />
+            </article>
+          </Fragment>
+        ))}
+      </div>
+      <Modal
+        show={activeImageIndex !== undefined}
+        size="xl"
+        onHide={close}
+        className={styles.modal}
+        fullscreen
+      >
+        <Modal.Header>
+          <Button onClick={close}>Close</Button>
+        </Modal.Header>
+        <Modal.Body className={styles.body}>
+          <Swiper
+            onSwiper={onSwiperCreation}
+            navigation
+            rewind
+            pagination
+            keyboard
+            mousewheel
+            modules={[Navigation, Pagination, Keyboard, Mousewheel]}
           >
-            <img className={styles.image} src={src} alt={description} />
-            <p className={styles.name}>{name}</p>
-          </article>
-          <Modal
-            show={activeImageId === id}
-            size="xl"
-            onHide={close}
-            className={styles.modal}
-            fullscreen
-          >
-            <Modal.Body className={styles.body}>
-              <img src={src} alt={description} className={styles.image} />
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={close}>Close</Button>
-              <Button onClick={previous}>Previous</Button>
-              <Button onClick={next}>Next</Button>
-            </Modal.Footer>
-          </Modal>
-        </Fragment>
-      ))}
-    </div>
+            {content.map(({ description, id, src }) => (
+              <SwiperSlide key={id} className={styles.slide}>
+                <img src={src} alt={description} className={styles.image} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
