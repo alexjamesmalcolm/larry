d2b0c6b694e7738f867e6e00ef8ea3ff801f0eca
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2022-05-19 18:41:00 -0500

    Added headshot to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,20 @@
-const About = () => {
-  return (
-    <p>
+import styles from "./About.module.css";
+import headshot from "../images/headshot.jpg";
+
+const About = () => (
+  <div className={styles.container}>
+    <img
+      src={headshot}
+      alt="Larry Mitchell in an orange beanie with a nice jean jacket"
+      className={styles.headshot}
+    />
+    <p className={styles.description}>
       Born in Cleveland, Ohio Larry moved to Columbus, Ohio to study Graphic
       Design and Fine Arts at the school of Columbus College of Art and Design.
       During this time Larry worked as a freelance Graphic Designer for both
       small and large companies while working at a local ice cream shop.
     </p>
-  );
-};
+  </div>
+);
 
 export default About;
