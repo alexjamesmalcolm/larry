55c5fbeb277dd799d75e03a6965e81d91b050a20
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2022-05-23 19:42:35 -0400

    Lazy loading Lightbox

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,22 +1,7 @@
-import { useCallback, useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { lazy, Suspense, useState } from "react";
 import styles from "./Gallery.module.css";
-import ISwiper, { Navigation, Pagination, Keyboard, Mousewheel } from "swiper";
-import { SwiperModule } from "swiper/types";
-import { useMediaQuery } from "react-responsive";
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-const swiperModules: SwiperModule[] = [
-  Navigation,
-  Pagination,
-  Keyboard,
-  Mousewheel,
-];
+const LightboxModal = lazy(() => import("./LightboxModal"));
 
 interface Source {
   id: string | number;
@@ -34,17 +19,6 @@ export interface Content {
 const Gallery = ({ content }: { content: Content[] }) => {
   const [activeImageIndex, setActiveImageIndex] = useState<number | undefined>(
     undefined
-  );
-  const close = useCallback(() => setActiveImageIndex(undefined), []);
-  const isSingleColumn = useMediaQuery({ maxWidth: "700px" });
-  const deviceHasFinePointer = useMediaQuery({ query: "(pointer: fine)" });
-  const onSwiperCreation = useCallback(
-    (swiper: ISwiper) => {
-      if (activeImageIndex) {
-        swiper.slideTo(activeImageIndex, 0);
-      }
-    },
-    [activeImageIndex]
   );
   return (
     <>
@@ -71,45 +45,15 @@ const Gallery = ({ content }: { content: Content[] }) => {
           </article>
         ))}
       </div>
-      <Modal
-        show={activeImageIndex !== undefined}
-        size="xl"
-        onHide={close}
-        className={styles.modal}
-        fullscreen
-      >
-        <Modal.Header>
-          <Button onClick={close}>Close</Button>
-        </Modal.Header>
-        <Modal.Body className={styles.body}>
-          <Swiper
-            onSwiper={onSwiperCreation}
-            navigation={deviceHasFinePointer}
-            rewind
-            pagination
-            keyboard
-            mousewheel
-            modules={swiperModules}
-            edgeSwipeDetection="prevent"
-            direction={isSingleColumn ? "vertical" : "horizontal"}
-          >
-            {content.map(({ description, id, legacySrc, sources = [] }) => (
-              <SwiperSlide key={id} className={styles.slide}>
-                <picture>
-                  {sources.map(({ src, media, id }) => (
-                    <source key={id} srcSet={src} media={media} />
-                  ))}
-                  <img
-                    className={styles.image}
-                    src={legacySrc}
-                    alt={description}
-                  />
-                </picture>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </Modal.Body>
-      </Modal>
+      {activeImageIndex !== undefined && (
+        <Suspense>
+          <LightboxModal
+            content={content}
+            activeImageIndex={activeImageIndex}
+            setActiveImageIndex={setActiveImageIndex}
+          />
+        </Suspense>
+      )}
     </>
   );
 };
