0f4683095807de85e237ed6c5c1eee46fd1cffe3
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2022-05-28 14:28:06 -0400

    Replaced png spinner with svg spinner

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,5 +1,5 @@
 import styles from "./Loading.module.css";
-import spinner from "../images/spinner.png";
+import spinner from "../images/rotate-solid.svg";
 
 const Loading = () => (
   <img src={spinner} alt="Loading indicator" className={styles.spinner} />
